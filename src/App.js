3651ce528e2e5600efcf9687d9da727358ae5494
Aneesh-Pothuru <54965062+Aneesh-Pothuru@users.noreply.github.com>
2020-12-18 21:09:18 -0800

    version 1.1.12

--- src/App.js
+++ src/App.js
@@ -44,18 +44,12 @@ class App extends React.Component {
     ).then(comment => {
       this.setState({ comments: comment });
       this.setState({ collectedData: true });
-      this.setState({ loading: false });
-      this.setState({ storeKey: true });
-      this.audio.pause();
-      this.audio = new Audio(song);
       console.log(this.state);
-    }).catch(err => {
-      console.log(err);
-      this.setState({ loading: false });
-      this.setState({ storeKey: true });
-      this.audio.pause();
-      this.audio = new Audio(song);
-    })
+    }).catch(err => console.log(err))
+    this.setState({ loading: false });
+    this.setState({ storeKey: true });
+    this.audio.pause();
+    this.audio = new Audio(song);
   }
 
   render() {
